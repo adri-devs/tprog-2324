9ff9d9f3befa5bb98885b7464b6fd0f74f7a27ba
adri-devs <71929914+adri-devs@users.noreply.github.com>
2024-04-11 08:57:58 +0200

    .

---
+++ practicas/practica4_816624_c++/shell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zeonp on 11/04/2024.
+//
+
+#include "shell.h"
---
+++ practicas/practica4_816624_c++/shell.h
@@ -0,0 +1,14 @@
+//
+// Created by zeonp on 11/04/2024.
+//
+
+#ifndef PRACTICA4_816624_C_SHELL_H
+#define PRACTICA4_816624_C_SHELL_H
+
+
+class shell {
+
+};
+
+
+#endif //PRACTICA4_816624_C_SHELL_H
