4951c079bf3b146d8455370093d84f21fef78bd4
Alejandro Lopez <845154@unizar.es>
2024-04-30 20:41:08 +0200

    .

--- practicas/practica5_816624_c++/LSystem.hs
+++ practicas/practica5_816624_c++/LSystem.hs
@@ -1,7 +1,8 @@
 -- Modulo independiente LSystem
 -- Autores: 816624, 845154
 -- Fecha últ mod: 30/04/24
-module Lsystem where
+module LSystem where
 lsystem :: (Char -> String) -> String -> Int -> String
 lsystem rules str 0 = str -- si el numero de iteraciones es 0, devuelve la cadena
-lsystem rules str n = lsystem rules (concatMap rules str)(n-1) -- concatena con concatMap recursivamente
+lsystem rules "" _ = "" -- si la cadena es vacia, devuelve una cadena vacia
+lsystem rules str n = lsystem rules (concatMap rules str) (n-1) -- concatena con concatMap recursivamente
--- practicas/practica5_816624_c++/main.hs
+++ practicas/practica5_816624_c++/main.hs
@@ -1,11 +1,27 @@
-import Lsystem
--- import Rules
+import LSystem
+import Rules
 import Plot
 import SVG
 
-pos = (1, 90, (0, 0), 90)
-args = ">+>+>+>+>"
-fig = tplot pos args
+turtle60 = (1,60,(0,0),0)
+turtle90 = (1,90,(0,0),0)
+turtle120 = (1,120,(0,0),0)
+
+lkoch = lsystem koch "F" 3
+lkochCuadrado = lsystem kochCuadrado "F" 4
+lkochSnowflake = lsystem kochSnowflake "F" 3
+lisla = lsystem isla "F" 3
+ltrianguloSierpinsky = lsystem trianguloSierpinsky "F" 3
+larrowHead = lsystem arrowHead "F" 6
+lhilbert = lsystem hilbert "F" 4
+lgosper = lsystem gosper "F" 3
 
 main = do
-    savesvg "square" fig
+    savesvg "koch" (tplot turtle60 lkoch)
+    savesvg "kochCuadrado" (tplot turtle90 lkochCuadrado)
+    savesvg "kochSnowflake" (tplot turtle60 lkochSnowflake)
+    savesvg "isla" (tplot turtle90 lisla)
+    savesvg "trianguloSierpinsky" (tplot turtle120 ltrianguloSierpinsky)
+    savesvg "arrowHead" (tplot turtle60 larrowHead)
+    savesvg "hilbert" (tplot turtle90 lhilbert)
+    savesvg "gosper" (tplot turtle60 lgosper)
